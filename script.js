1f8913856dff4cce8ecbcac341541464241a2784
noname <michael.check@outlook.com>
2022-05-30 19:31:46 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$('.multiple-items').slick({
+    //arrows: false,
+    infinite: false,
+    slidesToShow: 2.5,
+    slidesToScroll: 1,
+    prevArrow:$('.slick-prev'),
+    nextArrow:$('.slick-next')
+  });
